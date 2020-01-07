12661b048044d946d69b25e0349e6d1352e2b897
Naoki YAMAMOTO <yamamoto@val.co.jp>
2020-01-07 16:21:10 +0900

    チェックエラー時のメッセージで標柱の場合に停留所名が表示されるように修正した。

--- gtfstool/gtfs_check.c
+++ gtfstool/gtfs_check.c
@@ -29,6 +29,8 @@ static const char* get_stop_name(const char* stop_id)
     struct stop_t* stop;
 
     stop = (struct stop_t*)hash_get(g_gtfs_hash->stops_htbl, stop_id);
+    if (strlen(stop->parent_station) > 0)
+        stop = (struct stop_t*)hash_get(g_gtfs_hash->stops_htbl, stop->parent_station);
     return (stop)? stop->stop_name : stop_id;
 }
 
